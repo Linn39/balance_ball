056dfda071f14fbf90841ed823feae0c42eafb30
Lin Zhou <zhoulinn@yahoo.com>
2026-02-08 22:17:38 +0100

    Initial Commit

---
+++ balance_ball/balance_ballApp.swift
@@ -0,0 +1,17 @@
+//
+//  balance_ballApp.swift
+//  balance_ball
+//
+//  Created by Lin Zhou on 08.02.26.
+//
+
+import SwiftUI
+
+@main
+struct balance_ballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ balance_ballTests/balance_ballTests.swift
@@ -0,0 +1,17 @@
+//
+//  balance_ballTests.swift
+//  balance_ballTests
+//
+//  Created by Lin Zhou on 08.02.26.
+//
+
+import Testing
+@testable import balance_ball
+
+struct balance_ballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
